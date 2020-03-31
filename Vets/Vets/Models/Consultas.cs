d088e5aacac15dafe591ec225b5c01e48b51c884
Diogo Amendoeira <aluno21076@ipt.pt>
2020-03-31 16:13:00 +0100

    Adição das chaves forasteiras e das chave primárias nas tabelas

--- Vets/Vets/Models/Consultas.cs
+++ Vets/Vets/Models/Consultas.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,21 @@ namespace Vets.Models
 {
     public class Consultas
     {
+        [Key]
         public int ID { get; set; }
 
         public DateTime Data { get; set; }
 
         public string Observacoes { get; set; }
+
+        // FK para Animais
+        [ForeignKey(nameof(Animal))]
+        public int AnimalFK { get; set; } //Consulta -> Animal
+        public Animais Animal { get; set; }
+
+        //FK para Veterinários
+        [ForeignKey(nameof(Veterinario))]
+        public int VeterinarioFK { get; set; } //Consulta -> Veterinario
+        public Veterinarios Veterinario { get; set; }
     }
 }
